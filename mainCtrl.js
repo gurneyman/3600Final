36b9b32c3b326a082e97ca0e13bd9e7fb9f79089
Thomas Prince <tprince14@gmail.com>
2015-12-09 22:55:59 -0500

    Close menu fix

--- mainCtrl.js
+++ mainCtrl.js
@@ -2,4 +2,9 @@ angular.module('brovyn')
 .controller('mainCtrl', function( $scope, $window ){
   // CONTROLLER STUFF HERE
   $scope.showMenu = false;
+
+  function closeMenu() {
+    angular.element('.material-design-hamburger__icon--to-arrow').removeClass('.material-design-hamburger__icon--to-arrow').addClass('.material-design-hamburger__icon--from-arrow');
+    $scope.showMenu = false;
+  }
 });
